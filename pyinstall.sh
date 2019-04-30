dbcfbfb49593268d6b802feb84ef0f1c813fcbd1
date_xyg <34052991+97xyg@users.noreply.github.com>
2019-04-30 20:59:40 +0800

    Update pyinstall.sh

--- pyinstall.sh
+++ pyinstall.sh
@@ -9,5 +9,7 @@ cd Python-3.6.5
 make && make install
 ln -s /usr/local/python3/bin/python3 /usr/bin/python3
 ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3
+cd ~
+rm -f pyinstall.sh
 echo "脚本执行完成！"
 echo "输入python3 -V 验证python3是否安装成功，输入pip3 -V 验证匹配是否成功"
