e127f8563f19fb6f317896bd9a476084dce33f6d
date_xyg <34052991+97xyg@users.noreply.github.com>
2019-04-30 20:46:09 +0800

    Update pyinstall.sh

--- pyinstall.sh
+++ pyinstall.sh
@@ -4,7 +4,7 @@ yum install libffi-devel -y
 cd ~
 wget http://cdn.npm.taobao.org/dist/python/3.6.5/Python-3.6.5.tgz
 tar -zxvf Python-3.6.5.tgz
-cd Python-3.6.5.tgz
+cd Python-3.6.5
 ./configure prefix=/usr/local/python3
 make && make install
 ln -s /usr/local/python3/bin/python3 /usr/bin/python3
